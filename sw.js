37366966db2290279a6c147f9265d3614f7417db
pat888953 <donaldplayz123@gmail.com>
2026-07-16 06:48:00 -0700

    Rename route briefing button

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taxi-bo-v95";
+const CACHE_NAME = "taxi-bo-v96";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,8 +8,8 @@ const APP_SHELL = [
   "./four-in-one.css?v=2",
   "./four-in-one.js?v=2",
   "./phone.html",
-  "./styles.css?v=95",
-  "./app.js?v=95",
+  "./styles.css?v=96",
+  "./app.js?v=96",
   "./academy.js?v=93",
   "./settings.js?v=94",
   "./phone.js",
