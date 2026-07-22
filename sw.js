fc8094a28bd4dc78505f1d9b74e2cc33a289badd
pat888953 <donaldplayz123@gmail.com>
2026-07-21 21:21:32 -0700

    Simplify drive mode for phone screens

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taxi-bo-v104";
+const CACHE_NAME = "taxi-bo-v105";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,8 +8,8 @@ const APP_SHELL = [
   "./four-in-one.css?v=2",
   "./four-in-one.js?v=2",
   "./phone.html",
-  "./styles.css?v=103",
-  "./app.js?v=104",
+  "./styles.css?v=105",
+  "./app.js?v=105",
   "./academy.js?v=93",
   "./settings.js?v=94",
   "./phone.js",
