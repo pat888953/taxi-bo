4478a669cf8edbb94628fb933dc2363bf19da583
pat888953 <donaldplayz123@gmail.com>
2026-07-22 01:02:02 -0700

    Expand phone destination input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taxi-bo-v115";
+const CACHE_NAME = "taxi-bo-v116";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,8 +8,8 @@ const APP_SHELL = [
   "./four-in-one.css?v=2",
   "./four-in-one.js?v=2",
   "./phone.html",
-  "./styles.css?v=115",
-  "./app.js?v=115",
+  "./styles.css?v=116",
+  "./app.js?v=116",
   "./academy.js?v=93",
   "./settings.js?v=94",
   "./phone.js",
