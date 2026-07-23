7017342893edb05ef092ba6b68a984da2944557f
pat888953 <donaldplayz123@gmail.com>
2026-07-22 23:35:27 -0700

    Lower phone recorder utility bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "taxi-bo-v124";
+const CACHE_NAME = "taxi-bo-v125";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -8,8 +8,8 @@ const APP_SHELL = [
   "./four-in-one.css?v=2",
   "./four-in-one.js?v=2",
   "./phone.html",
-  "./styles.css?v=124",
-  "./app.js?v=124",
+  "./styles.css?v=125",
+  "./app.js?v=125",
   "./academy.js?v=93",
   "./settings.js?v=94",
   "./phone.js",
